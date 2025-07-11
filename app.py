6314f5d5f386e8231056921f0fe38d7ddd803d47
MANOJ KUMAR NEYYILA <manojkumarneyyila@gmail.com>
2025-07-11 16:50:07 +0530

    added cors middleware

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from routes.comment_router import router as comment_router
 from routes.user_router import router as user_router
 from middleware import AuthMiddleware
 from routes.post_router import router as post_router
+import os
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="StudentHub API", version="1.0.0")
 
@@ -11,6 +13,13 @@ app.add_middleware(AuthMiddleware)
 app.include_router(user_router, prefix="/api/v1/users", tags=["users"])
 app.include_router(post_router, prefix="/api/v1/posts", tags=["posts"])
 app.include_router(comment_router, prefix="/api/v1/comments", tags=["comments"])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins for development
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
