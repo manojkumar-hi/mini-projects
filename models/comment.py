55c75d2b16749dec920097f8d13c30757e4416ef
MANOJ KUMAR NEYYILA <manojkumarneyyila@gmail.com>
2025-07-22 17:53:59 +0530

    Update comment.py

--- models/comment.py
+++ models/comment.py
@@ -6,6 +6,5 @@ class Comment(BaseModel):
     comment_id: Optional[str] = None  # ✅ Make it optional with default None
     post_id: str
     content: str
-    file_url: Optional[str] = None
     created_by: Optional[str] = None   # ✅ Make sure this is optional
     created_at: Optional[datetime] = None  # ✅ Same here
